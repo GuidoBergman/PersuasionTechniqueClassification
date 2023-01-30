3d58e034297c35d0b1f0c47aad1b7fda01a93c24
imruhi <r.u.mahadeshwar@student.rug.nl>
2023-01-31 00:06:59 +0100

    added bar chart for excess/missing roles for tokens
    similar to the ref paper

--- model_trainers/utils/evaluate.py
+++ model_trainers/utils/evaluate.py
@@ -3,7 +3,7 @@ from datasets import Dataset
 
 from sklearn.metrics import classification_report, ConfusionMatrixDisplay, confusion_matrix
 import matplotlib.pyplot as plt
-
+import numpy as np
 from .torch_utils import class_vector_to_multi_hot_vector
 
 
@@ -16,8 +16,12 @@ def evaluate_model(ds: Dataset, inputs: list, labels: list, predictions: list) -
 
     label_list = ds["train"].features["verbnet"].feature.feature.names
 
+    # for confusion matrix
     predicted_labels_role = []
     true_labels_role = []
+
+    # for bar chart of percentage of missed/excess args
+    role_lengths = []
 
     # check if predictions are correct at position
     # does not pick from the top ones
@@ -27,18 +31,27 @@ def evaluate_model(ds: Dataset, inputs: list, labels: list, predictions: list) -
             true_labels_role.extend(ds["test"][idx]["verbnet"][i])
             possible_roles = pred[i]
             correct_pos = []
-            for role in test_roles_ordered[idx][i]:
-                if role in possible_roles:
+            # print(f"possible roles for {test_roles_ordered[idx][i]}: {possible_roles}")
+            for j, role in enumerate(test_roles_ordered[idx][i]):
+                # if theres missing roles in predictions add 0
+                if j >= len(possible_roles):
+                    predicted_labels_role.append(0)
+                    correct_pos.append(False)
+                # check if true role at current position matches 
+                # predicted role at curr position
+                elif role == possible_roles[j]:
                     correct_pos.append(True)
                     predicted_labels_role.append(role)
                 else:
-                    predicted_labels_role.append(0)
+                    predicted_labels_role.append(possible_roles[j])
                     correct_pos.append(False)
+            role_lengths.append(len(possible_roles) - len(test_roles_ordered[idx][i]))
             pred_order.append(correct_pos)
 
         # print(f"original sentence: {inputs[idx]}")
         # print(f"original labels: {ds['test'][idx]['verbnet']}")
         # print(f"Predicted labels correct?: {pred_order}")
+        # print(f"excess or misssing: {role_lengths}")
 
     token_labels = []
     token_preds = []
@@ -57,25 +70,54 @@ def evaluate_model(ds: Dataset, inputs: list, labels: list, predictions: list) -
 
     predicted_matrix = []
     true_matrix = []
-    matrix_labels = ["0", "Agent", "Asset", "Attribute", "Co-Theme",
-                     "Experiencer", "Location", "Theme", "PartOf", "Equal"]
+    matrix_labels = ["0", "Agent", "Attribute", "Co-Theme", "Destination", 
+                     "Experiencer", "Location", "Patient", "Theme", "PartOf", "Equal"]
 
     for idx, true in enumerate(true_labels_role):
         if true in matrix_labels:
             predicted_matrix.append(predicted_labels_role[idx])
             true_matrix.append(true_labels_role[idx])
 
-    # true-pred matrix for certain labels
-    matrix = confusion_matrix(
-        true_matrix, predicted_matrix, labels=matrix_labels, normalize='true')
-    cm_display = ConfusionMatrixDisplay(
-        confusion_matrix=matrix, display_labels=matrix_labels)
 
-    # true-pred matrix for all labels
-    # matrix = confusion_matrix(true_labels_role, predicted_labels_role, labels = label_list, normalize='true')
-    # cm_display = ConfusionMatrixDisplay(confusion_matrix = matrix, display_labels = label_list)
+    # true-pred confusion matrix for certain labels 
+    matrix_1 = confusion_matrix(true_matrix, predicted_matrix, labels=matrix_labels, normalize='true')
+    cm_display = ConfusionMatrixDisplay(confusion_matrix=matrix_1, display_labels=matrix_labels)
 
+    plt.rcParams.update({'axes.labelsize': 20, 'xtick.labelsize': 12, 'ytick.labelsize': 12})
     cm_display.plot(cmap=plt.cm.YlGn)
     plt.xticks(rotation=90)
-    #plt.savefig()
-    #plt.show()
+    plt.show()
+
+    # true-pred confusion matrix for all labels
+    matrix_2 = confusion_matrix(true_labels_role, predicted_labels_role, labels=label_list, normalize='true')
+    cm_display = ConfusionMatrixDisplay(confusion_matrix=matrix_2, display_labels=label_list)
+    cm_display.plot(cmap=plt.cm.YlGn)
+    plt.xticks(rotation=90)
+    plt.show()
+
+    # bar chart of percentage of missed/excess args
+    labels = ['1', '2', '3+']
+
+    perc_excess_1 = (len([y for y in role_lengths if y == 1])/len(role_lengths)) * 100
+    perc_excess_2 = (len([y for y in role_lengths if y == 2])/len(role_lengths)) * 100
+    perc_excess_3ab = (len([y for y in role_lengths if y >= 3])/len(role_lengths)) * 100
+    perc_missing_1 = (len([y for y in role_lengths if y == -1])/len(role_lengths)) * 100
+    perc_missing_2 = (len([y for y in role_lengths if y == -2])/len(role_lengths)) * 100
+    perc_missing_3ab = (len([y for y in role_lengths if y <= -3])/len(role_lengths)) * 100
+
+    excess = [perc_excess_1, perc_excess_2, perc_excess_3ab]
+    missing = [perc_missing_1, perc_missing_2, perc_missing_3ab]
+
+    x = np.arange(len(labels))
+    width = 0.35
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width/2, missing, width, label='Missed')
+    rects2 = ax.bar(x + width/2, excess, width, label='Excess')
+    ax.set_ylabel("% of tokens")
+    ax.set_xticks(x, labels)
+    ax.legend()
+    ax.bar_label(rects1, padding=3)
+    ax.bar_label(rects2, padding=3)
+    fig.tight_layout()
+    plt.show()
+    
