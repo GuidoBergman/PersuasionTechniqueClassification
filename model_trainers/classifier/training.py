7fca3986380a1db466d130b56bfbf2af5d69a46b
Guido Bergman <gbergman@fi.uba.ar>
2024-04-02 19:50:19 -0300

    Fix errors

--- model_trainers/classifier/training.py
+++ model_trainers/classifier/training.py
@@ -134,13 +134,9 @@ def train_classifier(dataset: Dataset, model_name: str, output_dir: str,
             #pred = flat_outputs.heaviside(torch.tensor(
                 #[0.0], device=device)).int().tolist()
 
-            print('Pred after sigmoid: ', outputs)
-            pred = torch.sigmoid(outputs).cpu().detach().numpy().tolist()
-            print('Pred after sigmoid: ', pred)
-            pred = np.array(outputs) >= evaluation_threshold
-            print('Pred after filter: ', pred)
+            pred = torch.sigmoid(flat_outputs)#.cpu().detach().numpy().tolist()
+            pred = (np.array(pred) >= evaluation_threshold).astype(int)
             true_label = flat_labels.int().tolist()
-            print('True lables:', true_label)
 
             preds.extend(pred)
             true_labels.extend(true_label)
